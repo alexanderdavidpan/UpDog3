671aa40d7e0d6218621f45e131cf4846a0af1981
Alex Pan <alexanderdavidpan@gmail.com>
2016-11-04 18:31:47 -0700

    Add basic client-side chat functionality.
    - Add view and CSS for primitive chat box.
    - Create messages model.

--- client/main.js
+++ client/main.js
@@ -1,22 +1,37 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
+//
+// import './main.html';
+import { Meteor } from 'meteor/meteor';
 
-import './main.html';
+/**
+* Templates
+*/
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+if (Meteor.isClient) {
+  Template.messages.helpers({
+      messages: function() {
+          return Messages.find({}, { sort: { time: 1}});
+      }
+  });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+  Template.input.events = {
+    'keydown input#message' : function (event) {
+      if (event.which == 13) { // 13 is the enter key event
+        var name = 'Anonymous';
+        var message = document.getElementById('message');
+        if (message.value != '') {
+          Messages.insert({
+            name: name,
+            message: message.value,
+            time: Date.now(),
+          });
+
+          document.getElementById('message').value = '';
+          message.value = '';
+        }
+      }
+    }
+  }
+}
